cdac96c2d7c416f814a42d66987b81a0c7b3c8d3
eight <eight04@gmail.com>
2017-07-29 20:00:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+Better Standalone Image
+=======================
+
+An userscript to make standalone image display better.
+
+Features
+--------
+
+* Style standalone image page. Make it center and background transparent.
+* Support SVG (Currently Greasemonkey-only).
+* No configuration. Please edit the source if you prefer a different color.
+
+Screenshot
+----------
+
+
+
+Compatibility
+-------------
+
+* Firefox + Greasemonkey 3
+* Chrome + Tampermonkey
+	- Tampermonkey doesn't execute userscripts on SVG images.
+	
+Install
+-------
+
+GreasyFork
+
+Similar scripts
+---------------
+
+* [Standalon Image Background and Transparency](https://greasyfork.org/zh-TW/scripts/63-standalone-image-background-and-transparency)
+* [Center Image](https://greasyfork.org/zh-TW/scripts/110-center-image)
+* [Advanced Image Viewer](https://greasyfork.org/zh-TW/scripts/27738-advanced-image-viewer)
+
+Known Issues
+------------
+
+* Vertical centering doesn't work well with SVG. Some parts might be cropped if the image is larger than windows.
---
+++ better-standalone-image.user.js
@@ -0,0 +1,59 @@
+// ==UserScript==
+// @name Better Standalone Image
+// @version 0.0.0
+// @description Make standalone image display better
+// @license MIT
+// @author eight04 <eight04@gmail.com>
+// @homepageURL https://github.com/eight04/better-standalone-image
+// @supportURL https://github.com/eight04/better-standalone-image/issues
+// @namespace eight04.blogspot.com
+// @include *
+// @run-at document-start
+// @grant GM_getResourceURL
+// @grant GM_getResourceText
+// @grant GM_addStyle
+// @resource style https://rawgit.com/eight04/better-standalone-image/master/better-standalone-image.user.css
+// @compatible firefox Greasemonkey 3
+// @compatible chrome Tampermonkey
+// ==/UserScript==
+
+(function() {
+	if (!document.contentType.startsWith("image")) return;
+	
+	if (document.contentType == "image/svg+xml") {
+		const s = document.documentElement.style;
+		s.background = `
+			linear-gradient(
+				45deg, rgba(0, 0, 0, .25), rgba(0, 0, 0, .25) 25%,
+				transparent 25%, transparent 75%, rgba(0, 0, 0, .25) 75%
+			),
+			linear-gradient(
+				45deg, rgba(0, 0, 0, .25), rgba(0, 0, 0, .25) 25%,
+				transparent 25%, transparent 75%, rgba(0, 0, 0, .25) 75%
+			),
+			#222
+		`;
+		s.backgroundPosition = "0 0, 8px 8px";
+		s.backgroundSize = "16px 16px";
+		
+		s.position = "absolute";
+		s.top = "0";
+		s.right = "0";
+		s.bottom = "0";
+		s.left = "0";
+		s.margin = "auto";
+		
+		s.boxShadow = "0 0 10px 10px rgba(0, 0, 0, .4)";
+	} else {
+		if (GM_info.scriptHandler == "Tampermonkey") {
+			// Use GM_addStyle to bypass CSP in tampermonkey
+			GM_addStyle(GM_getResourceText("style"));
+		} else {
+			// Use extension protocol to bypass CSP in greasemonkey
+			const link = document.createElement("link");
+			link.href = GM_getResourceURL("style");
+			link.rel = "stylesheet";
+			document.documentElement.appendChild(link);
+		}
+	}
+})();
